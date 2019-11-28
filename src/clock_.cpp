8287f662e7c51f6d4a6cb22cc40566ec2acb1306
fakeacount123 <58228292+fakeacount123@users.noreply.github.com>
2019-11-28 11:21:03 -0400

    Add files via upload
    READ-ME TEXT:
    
    ALUNO	: JOSÉ PAULO CAMILOTTI DE OLIVEIRA
    RGA	: 2016.1905.017-0
    
    EXECUTAR NORMALMENTE, NÃO HOUVERAM ALTERAÇÃO NO PROCESSO DE EXECUÇÃO.

--- src/clock_.cpp
+++ src/clock_.cpp
@@ -1,18 +1,36 @@
 #include "clock_.hpp"
+#include <string>       // std::string
+#include <iostream>     // std::cout
+#include <sstream>
 
 clock_::clock_(sc_module_name name, int dl, nana::label &clk):  sc_module(name), delay(dl), clock_count(clk)
 {
     SC_THREAD(main);
 }
 
+int string_to_int(std::string clock_ns)
+{
+	int clock_int = std::stoi(clock_ns);
+	return clock_int;
+}
+
 void clock_::main()
 {
     while(true)
-    {
+    {	
+    	int clock_int, instructions_count, IPC;
         sc_pause();
         wait(SC_ZERO_TIME);
         out->write("");
         clock_count.caption(sc_time_stamp().to_string());
+        std::cout << "### Clock atual		: " << sc_time_stamp().to_string() << ".\n";
+        clock_int = string_to_int(sc_time_stamp().to_string());
+        instructions_count = clock_int;
+        std::cout << "### Instrucao atual	: " << instructions_count << ".\n";
+        if(clock_int != 0){
+        	IPC = instructions_count/clock_int;
+        	std::cout << "### IPC atual		: " << IPC << " instrucoes por ciclo.\n";
+        }
         wait(delay,SC_NS);
     }
 }
